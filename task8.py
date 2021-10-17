4236e7f8dee93fe7b2f50785e81d63c0be12d901
Siddhant Srivastava <sidsrivast42@gmail.com>
2021-10-17 13:57:52 -0700

    taking individual simp from 3 and 4

--- task8.py
+++ task8.py
@@ -1,5 +1,13 @@
 import pickle
 
+
 def start_task8():
     with open('metadata.pickle', 'rb') as handle:
         metadata = pickle.load(handle)
+
+    with open('simp.pickle', 'rb') as handle:
+        simp = pickle.load(handle)
+    print('here')
+
+if __name__ == '__main__':
+    start_task8()
