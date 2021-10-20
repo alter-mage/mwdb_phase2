a44b5651695ff895329d172bfe0bce2840cbc15a
shivansh1905 <shivansh1905@gmail.com>
2021-10-19 19:42:36 -0700

    Few things here and there

--- color_moment_similarity.py
+++ color_moment_similarity.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-
 def get_similarity(x1, x2):
-    return 1/(np.linalg.norm(np.subtract(x1, x2),ord=1))
+    similarities = []
+    for row in x2:
+        similarities.append(1/(np.linalg.norm(np.subtract(x1, row),ord=1)))
+    return similarities
  
